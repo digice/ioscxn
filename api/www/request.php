33786e87ab6058ff01bb438a30e9e1ca970f526d
Roderic Linguri <linguri@digices.com>
2017-06-06 14:49:43 -0600

    Create request.php

---
+++ api/www/request.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+  * @package   iOSCxn
+  * @author    Roderic Linguri <linguri@digices.com>
+  * @copyright 2017 Digices LLC
+  * @license   https://digices.com/license/mit.txt
+  **/
+
+/** iOSCxn Bootstrapper **/
+
+namespace ioscxn;
+
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'autoload.php';
+
+$app = new \ioscxn\App();
