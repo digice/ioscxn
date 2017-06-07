f25bf111cd7552a133f827edaef37566936485b3
Roderic Linguri <linguri@digices.com>
2017-06-06 18:10:30 -0600

    Update config.php

--- api/etc/config.php
+++ api/etc/config.php
@@ -1,4 +1,13 @@
 <?php
 
+/**
+ * @package   iOSCxn
+ * @author    Roderic Linguri
+ * @copyright 2017 Digices LLC
+ * @license   https://github.com/digice/<#package#>/blob/master/LICENSE
+ **/
+ 
 /** iOSCxn Configuration **/
+
+namespace ioscxn;
 
